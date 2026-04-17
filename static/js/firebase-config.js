721722880b895198745ab17f74d9471b036ea902
Auto Deploy <deploy@example.com>
2026-04-17 17:37:19 +0530

    Integrated Firebase SDK and hosting config

---
+++ static/js/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration from your dashboard
+const firebaseConfig = {
+  apiKey: "AIzaSyBK263TodJRlMuj2nM3fWL-TbBNZ1EeFy8",
+  authDomain: "projecthub-d7bfe.firebaseapp.com",
+  projectId: "projecthub-d7bfe",
+  storageBucket: "projecthub-d7bfe.firebasestorage.app",
+  messagingSenderId: "897437426423",
+  appId: "1:897437426423:web:c20faa30a7fea12dd0e9f2",
+  measurementId: "G-9SPXS2484E"
+};
+
+// This will be used in the browser
+export default firebaseConfig;
